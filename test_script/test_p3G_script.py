1b91d0de3e1d9171555320e5268acb08f9ba4ea1
beccawilson <rebecca.c.wilson@gmail.com>
2014-10-09 15:14:13 +0100

    started string manipulation to remove <br> from text

--- test_script/test_p3G_script.py
+++ test_script/test_p3G_script.py
@@ -9,7 +9,7 @@ import csv
 import glob, os
 
 #set working directory
-#os.chdir("/home/rw13742/Documents/ecouter/p3G_ecouter/data_extractor")
+os.chdir("/home/rw13742/Documents/ecouter/ecouter/test_script")
 
 #chnage file extension to .json
 for filename in glob.iglob(os.path.join(os.getcwd(), '*.m42')):
@@ -18,6 +18,23 @@ for filename in glob.iglob(os.path.join(os.getcwd(), '*.m42')):
 #open file
 mm_file=open('1411460991.json')
 mm=json.load(mm_file)
+
+##############################################
+#test string find and replace to remove <br>
+
+#def function replace_all 
+#def replace_all(text, dic):
+#    for i, j in dic.iteritems():
+#        text = text.replace(i, j)
+#    return text
+
+#text = 'hello <br>everybody'
+#find_dic = {'<br>':' '}
+#text = replace_all(text, find_dic)
+#print text  
+
+####what to do about spaces?
+##############################################
 
 #prints root_text i.e. mind map title
 #root_text = mm["root"]["attributes"]
@@ -39,6 +56,7 @@ def print_children2(node,parent):
 	text = this_one["attributes"]["text"]
 	note = this_one["attributes"]["note"]
 	links = this_one["attributes"]["links"]
+	links = links.values()
 	image = this_one["attributes"]["image"]
 	last_editor = this_one["attributes"]["lastEditor"]
 	last_edit = datetime.datetime.fromtimestamp(this_one["attributes"]["lastEdit"]/1000).strftime('%Y-%m-%d %H:%M:%S')
@@ -47,15 +65,18 @@ def print_children2(node,parent):
 	
 	with open("ouput.csv", "ab") as csvfile:
     	    output_file = csv.writer(csvfile)
-	    output_file.writerow([ID, parent, text.encode('utf-8'), note, links, image, last_editor, last_edit]) #add to this list of columns
+	    output_file.writerow([ID, parent, text.encode('utf-8'), note, links.encode('utf-8'), image, last_editor, last_edit]) #add to this list of columns
 	
+
 with open("ouput.csv", "ab") as csvfile:
     output_file = csv.writer(csvfile)
     output_file.writerow(["ID", "parent", "text", "note", "links", "image", "last_editor", "last_edit"]) 
   
+
 root_text = mm["root"]["attributes"]
 parent = root_text.values()[0]
 print_children2(mm["root"]["children"],parent)
+
 
         
 
